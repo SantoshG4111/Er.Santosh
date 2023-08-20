eb78fdb29d64cd8804e6016c25716e30547e3b08
santosh kumar <132646103+Sk4111@users.noreply.github.com>
2023-08-20 11:52:19 +0530

    Add files via upload

---
+++ portfolio website/script.js
@@ -0,0 +1,35 @@
+@media(max-width:700px){
+    .text-box h1{
+        font-size: 42px;
+    }
+.nav-links ul li{
+    display: block;
+   }
+   .nav-links{
+    position: absolute;
+    background: #04a354;
+    height: 100vh;
+    width: 200px;
+    top: 0;
+    right:-300px;
+    text-align: left;
+    z-index: 2;
+  }
+  nav.fa{
+    display: block;
+    color: #f44336;
+    margin: 10px;
+    font-size: 22px;
+    cursor: pointer;
+}
+.nav-links ul{
+    padding: 30px;
+}
+font-family: 'Poppins', sans-serif;
+
+
+
+
+ 
+
+
